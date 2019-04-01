1e09a7cddca3a851f9a9fa5851bc0a55f514a9cb
Andre Lussier <Dremon7@hotmail.com>
2019-04-01 14:24:30 -0400

    added aspects for login

---
+++ src/main/java/guru/springframework/services/security/LoginAspect.java
@@ -0,0 +1,36 @@
+package guru.springframework.services.security;
+
+
+import org.apache.tomcat.util.net.jsse.openssl.Authentication;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoginAspect {
+
+    @Pointcut("execution(* org.springframework.security.authentication.AuthenticationProvider.authenticate(..))")
+    public void doAuthenticate(){
+
+    }
+
+    @Before("guru.springframework.services.security.LoginAspect.doAuthenticate() && args(authentication)")
+    public void logBefore(Authentication authentication){
+
+        System.out.println("This is before the Authenticate Method: authentication: " + authentication.isAuthenticated());
+    }
+
+    @AfterReturning(value = "guru.springframework.services.security.LoginAspect.doAuthenticate()",
+            returning = "authentication")
+    public void logAfterAuthenticate( Authentication authentication){
+        System.out.println("This is after the Authenticate Method authentication: " + authentication.isAuthenticated());
+    }
+
+    @AfterThrowing("guru.springframework.services.security.LoginAspect.doAuthenticate() && args(authentication)")
+    public void logAuthenicationException(Authentication authentication){
+        String userDetails = (String) authentication.getPrincipal();
+        System.out.println("Login failed for user: " + userDetails);
+    }
+
+
+}
